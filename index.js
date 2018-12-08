cdb769157fff92efddf2962553495781a18f1af8
marcusbass323 <marcus@bassconsultantgroup.com>
2018-12-08 11:13:45 -0800

    Get endpoint showing projects

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const server = express();
+const helmet = require('helmet');
+const logger = require('morgan');
+const db = require('./data/helpers/actionModel')
 const PORT = 7000;
 
+server.use(
+    logger('dev'),
+    helmet(),
+)
+
+//ENDPOINTS
+
+//GET ENDPOINT
+server.get('/projects', (req, res) => {
+    db.get()
+        .then(projects => {
+            res.json(projects)
+        })
+        .catch(err => {
+            res
+            .status(500)
+            .json({
+                message: "Cound not fetch the projects."
+            })
+        })
+})
 
 
 server.listen(PORT, () => {
